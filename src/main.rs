6bbb0be4e3ab4ba488337b807f4b721608ceca72
JP Arimaa <juhapekka.arimaa@gmail.com>
2021-07-05 21:37:52 +0300

    Add vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use vulkano::instance::Instance;
+use vulkano::instance::InstanceExtensions;
+use vulkano::instance::PhysicalDevice;
+
 fn main() {
-    println!("Hello, world!!!");
+    let instance =
+        Instance::new(None, &InstanceExtensions::none(), None).expect("failed to create instance");
+    let physical_device = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("no device available");
 }
