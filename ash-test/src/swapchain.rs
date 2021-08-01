d5deaf893160eb64731961069da654044c68c3f3
JP Arimaa <juhapekka.arimaa@gmail.com>
2021-08-01 20:25:33 +0300

    Ash: create image views for swapchain

--- ash-test/src/swapchain.rs
+++ ash-test/src/swapchain.rs
@@ -1,12 +1,5 @@
+use ash::version::DeviceV1_0;
 use ash::vk;
-
-pub struct Swapchain {
-    pub loader: ash::extensions::khr::Swapchain,
-    pub vk_swapchain_khr: vk::SwapchainKHR,
-    pub images: Vec<vk::Image>,
-    pub format: vk::Format,
-    pub extent: vk::Extent2D,
-}
 
 pub struct SwapchainSupportInfo {
     pub capabilities: vk::SurfaceCapabilitiesKHR,
@@ -36,62 +29,86 @@ pub fn get_swapchain_support_info(
     }
 }
 
-pub fn create_swapchain(
-    instance: &ash::Instance,
-    device: &ash::Device,
-    physical_device: vk::PhysicalDevice,
-    surface: &crate::surface::Surface,
-) -> Swapchain {
-    let swapchain_support_info = get_swapchain_support_info(physical_device, surface);
-    let surface_format = choose_swapchain_format(&swapchain_support_info.formats);
-    let present_mode = choose_swapchain_present_mode(&swapchain_support_info.present_modes);
-    let extent = choose_swapchain_extent(&swapchain_support_info.capabilities);
+pub struct Swapchain {
+    pub loader: ash::extensions::khr::Swapchain,
+    pub vk_swapchain_khr: vk::SwapchainKHR,
+    pub images: Vec<vk::Image>,
+    pub format: vk::Format,
+    pub extent: vk::Extent2D,
+    pub image_views: Vec<vk::ImageView>,
+}
 
-    let image_count = std::cmp::min(
-        swapchain_support_info.capabilities.max_image_count,
-        swapchain_support_info.capabilities.min_image_count + 1,
-    );
+impl Swapchain {
+    pub fn new(
+        instance: &ash::Instance,
+        device: &ash::Device,
+        physical_device: vk::PhysicalDevice,
+        surface: &crate::surface::Surface,
+    ) -> Swapchain {
+        let swapchain_support_info = get_swapchain_support_info(physical_device, surface);
+        let surface_format = choose_swapchain_format(&swapchain_support_info.formats);
+        let present_mode = choose_swapchain_present_mode(&swapchain_support_info.present_modes);
+        let extent = choose_swapchain_extent(&swapchain_support_info.capabilities);
 
-    let swapchain_create_info = vk::SwapchainCreateInfoKHR {
-        s_type: vk::StructureType::SWAPCHAIN_CREATE_INFO_KHR,
-        p_next: std::ptr::null(),
-        flags: vk::SwapchainCreateFlagsKHR::empty(),
-        surface: surface.vk_surface_khr,
-        min_image_count: image_count,
-        image_color_space: surface_format.color_space,
-        image_format: surface_format.format,
-        image_extent: extent,
-        image_usage: vk::ImageUsageFlags::COLOR_ATTACHMENT,
-        image_sharing_mode: vk::SharingMode::EXCLUSIVE,
-        p_queue_family_indices: std::ptr::null(),
-        queue_family_index_count: 0,
-        pre_transform: swapchain_support_info.capabilities.current_transform,
-        composite_alpha: vk::CompositeAlphaFlagsKHR::OPAQUE,
-        present_mode,
-        clipped: vk::TRUE,
-        old_swapchain: vk::SwapchainKHR::null(),
-        image_array_layers: 1,
-    };
+        let image_count = std::cmp::min(
+            swapchain_support_info.capabilities.max_image_count,
+            swapchain_support_info.capabilities.min_image_count + 1,
+        );
 
-    let swapchain_loader = ash::extensions::khr::Swapchain::new(instance, device);
-    let vk_swapchain_khr = unsafe {
-        swapchain_loader
-            .create_swapchain(&swapchain_create_info, None)
-            .expect("Failed to create Swapchain!")
-    };
+        let swapchain_create_info = vk::SwapchainCreateInfoKHR {
+            s_type: vk::StructureType::SWAPCHAIN_CREATE_INFO_KHR,
+            p_next: std::ptr::null(),
+            flags: vk::SwapchainCreateFlagsKHR::empty(),
+            surface: surface.vk_surface_khr,
+            min_image_count: image_count,
+            image_color_space: surface_format.color_space,
+            image_format: surface_format.format,
+            image_extent: extent,
+            image_usage: vk::ImageUsageFlags::COLOR_ATTACHMENT,
+            image_sharing_mode: vk::SharingMode::EXCLUSIVE,
+            p_queue_family_indices: std::ptr::null(),
+            queue_family_index_count: 0,
+            pre_transform: swapchain_support_info.capabilities.current_transform,
+            composite_alpha: vk::CompositeAlphaFlagsKHR::OPAQUE,
+            present_mode,
+            clipped: vk::TRUE,
+            old_swapchain: vk::SwapchainKHR::null(),
+            image_array_layers: 1,
+        };
 
-    let images = unsafe {
-        swapchain_loader
-            .get_swapchain_images(vk_swapchain_khr)
-            .expect("Failed to get swapchain images")
-    };
+        let swapchain_loader = ash::extensions::khr::Swapchain::new(instance, device);
+        let vk_swapchain_khr = unsafe {
+            swapchain_loader
+                .create_swapchain(&swapchain_create_info, None)
+                .expect("Failed to create Swapchain!")
+        };
 
-    Swapchain {
-        loader: swapchain_loader,
-        vk_swapchain_khr,
-        format: surface_format.format,
-        extent,
-        images,
+        let images = unsafe {
+            swapchain_loader
+                .get_swapchain_images(vk_swapchain_khr)
+                .expect("Failed to get swapchain images")
+        };
+
+        let image_views = create_image_views(device, surface_format.format, &images);
+
+        Swapchain {
+            loader: swapchain_loader,
+            vk_swapchain_khr,
+            format: surface_format.format,
+            extent,
+            images,
+            image_views,
+        }
+    }
+
+    pub fn destroy(&mut self, device: &ash::Device) {
+        unsafe {
+            for &imageview in self.image_views.iter() {
+                device.destroy_image_view(imageview, None);
+            }
+
+            self.loader.destroy_swapchain(self.vk_swapchain_khr, None);
+        }
     }
 }
 
@@ -134,4 +151,45 @@ fn choose_swapchain_extent(capabilities: &vk::SurfaceCapabilitiesKHR) -> vk::Ext
             ),
         }
     }
+}
+
+fn create_image_views(device: &ash::Device, surface_format: vk::Format, images: &Vec<vk::Image>) -> Vec<vk::ImageView> {
+    let mut swapchain_imageviews = vec![];
+
+    let components = vk::ComponentMapping {
+        r: vk::ComponentSwizzle::IDENTITY,
+        g: vk::ComponentSwizzle::IDENTITY,
+        b: vk::ComponentSwizzle::IDENTITY,
+        a: vk::ComponentSwizzle::IDENTITY,
+    };
+
+    let subresource_range = vk::ImageSubresourceRange {
+        aspect_mask: vk::ImageAspectFlags::COLOR,
+        base_mip_level: 0,
+        level_count: 1,
+        base_array_layer: 0,
+        layer_count: 1,
+    };
+
+    for &image in images.iter() {
+        let imageview_create_info = vk::ImageViewCreateInfo {
+            s_type: vk::StructureType::IMAGE_VIEW_CREATE_INFO,
+            p_next: std::ptr::null(),
+            flags: vk::ImageViewCreateFlags::empty(),
+            view_type: vk::ImageViewType::TYPE_2D,
+            format: surface_format,
+            components,
+            subresource_range,
+            image,
+        };
+
+        let imageview = unsafe {
+            device
+                .create_image_view(&imageview_create_info, None)
+                .expect("Failed to create image view")
+        };
+        swapchain_imageviews.push(imageview);
+    }
+
+    swapchain_imageviews
 }
--- ash-test/src/vulkan_app.rs
+++ ash-test/src/vulkan_app.rs
@@ -17,9 +17,7 @@ pub struct VulkanApp {
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
-            self.swapchain
-                .loader
-                .destroy_swapchain(self.swapchain.vk_swapchain_khr, None);
+            self.swapchain.destroy(&self.device);
             self.device.destroy_device(None);
             self.surface.loader.destroy_surface(self.surface.vk_surface_khr, None);
             self.debug_utils
@@ -42,7 +40,7 @@ impl VulkanApp {
         let surface = crate::surface::Surface::new(&entry, &instance, window);
         let (physical_device, queue_families) = crate::physical_device::get_physical_device(&instance, &surface);
         let device = crate::device::create_logical_device(&instance, physical_device, &queue_families);
-        let swapchain = crate::swapchain::create_swapchain(&instance, &device, physical_device, &surface);
+        let swapchain = crate::swapchain::Swapchain::new(&instance, &device, physical_device, &surface);
 
         VulkanApp {
             _entry: entry,
