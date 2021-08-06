fe01ccae99d5a9c9e69b6e999e3346cc10509b76
JP Arimaa <juhapekka.arimaa@gmail.com>
2021-08-06 20:52:47 +0300

    Ash: create render pass and graphics pipeline

--- ash-test/src/vulkan_app.rs
+++ ash-test/src/vulkan_app.rs
@@ -12,11 +12,17 @@ pub struct VulkanApp {
     device: ash::Device,
     _queue_families: crate::utility::QueueFamilyIndices,
     swapchain: crate::swapchain::Swapchain,
+    render_pass: vk::RenderPass,
+    pipeline_layout: vk::PipelineLayout,
+    graphics_pipeline: vk::Pipeline,
 }
 
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
+            self.device.destroy_pipeline(self.graphics_pipeline, None);
+            self.device.destroy_pipeline_layout(self.pipeline_layout, None);
+            self.device.destroy_render_pass(self.render_pass, None);
             self.swapchain.destroy(&self.device);
             self.device.destroy_device(None);
             self.surface.loader.destroy_surface(self.surface.vk_surface_khr, None);
@@ -41,7 +47,8 @@ impl VulkanApp {
         let (physical_device, queue_families) = crate::physical_device::get_physical_device(&instance, &surface);
         let device = crate::device::create_logical_device(&instance, physical_device, &queue_families);
         let swapchain = crate::swapchain::Swapchain::new(&instance, &device, physical_device, &surface);
-        create_graphics_pipeline(&device);
+        let render_pass = create_render_pass(&device, swapchain.format);
+        let (pipeline_layout, graphics_pipeline) = create_graphics_pipeline(&device, render_pass, swapchain.extent);
 
         VulkanApp {
             _entry: entry,
@@ -53,6 +60,9 @@ impl VulkanApp {
             device,
             _queue_families: queue_families,
             swapchain,
+            render_pass,
+            pipeline_layout,
+            graphics_pipeline,
         }
     }
 
@@ -61,7 +71,11 @@ impl VulkanApp {
     }
 }
 
-fn create_graphics_pipeline(device: &ash::Device) {
+fn create_graphics_pipeline(
+    device: &ash::Device,
+    render_pass: vk::RenderPass,
+    swapchain_extent: vk::Extent2D,
+) -> (vk::PipelineLayout, vk::Pipeline) {
     use std::path::Path;
     let vertex_shader_code = crate::utility::read_file(Path::new("ash-test/shader_bin/vert.spv"));
     let fragment_shader_code = crate::utility::read_file(Path::new("ash-test/shader_bin/frag.spv"));
@@ -71,7 +85,7 @@ fn create_graphics_pipeline(device: &ash::Device) {
 
     let main_function_name = std::ffi::CString::new("main").unwrap();
 
-    let _shader_stages = [
+    let shader_stages = [
         vk::PipelineShaderStageCreateInfo {
             s_type: vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
             p_next: std::ptr::null(),
@@ -92,8 +106,224 @@ fn create_graphics_pipeline(device: &ash::Device) {
         },
     ];
 
+    let vertex_input_state_create_info = vk::PipelineVertexInputStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineVertexInputStateCreateFlags::empty(),
+        vertex_attribute_description_count: 0,
+        p_vertex_attribute_descriptions: std::ptr::null(),
+        vertex_binding_description_count: 0,
+        p_vertex_binding_descriptions: std::ptr::null(),
+    };
+
+    let vertex_input_assembly_state_info = vk::PipelineInputAssemblyStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        flags: vk::PipelineInputAssemblyStateCreateFlags::empty(),
+        p_next: std::ptr::null(),
+        primitive_restart_enable: vk::FALSE,
+        topology: vk::PrimitiveTopology::TRIANGLE_LIST,
+    };
+
+    let viewports = [vk::Viewport {
+        x: 0.0,
+        y: 0.0,
+        width: swapchain_extent.width as f32,
+        height: swapchain_extent.height as f32,
+        min_depth: 0.0,
+        max_depth: 1.0,
+    }];
+
+    let scissors = [vk::Rect2D {
+        offset: vk::Offset2D { x: 0, y: 0 },
+        extent: swapchain_extent,
+    }];
+
+    let viewport_state_create_info = vk::PipelineViewportStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineViewportStateCreateFlags::empty(),
+        scissor_count: scissors.len() as u32,
+        p_scissors: scissors.as_ptr(),
+        viewport_count: viewports.len() as u32,
+        p_viewports: viewports.as_ptr(),
+    };
+
+    let rasterization_statue_create_info = vk::PipelineRasterizationStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineRasterizationStateCreateFlags::empty(),
+        depth_clamp_enable: vk::FALSE,
+        cull_mode: vk::CullModeFlags::BACK,
+        front_face: vk::FrontFace::CLOCKWISE,
+        line_width: 1.0,
+        polygon_mode: vk::PolygonMode::FILL,
+        rasterizer_discard_enable: vk::FALSE,
+        depth_bias_clamp: 0.0,
+        depth_bias_constant_factor: 0.0,
+        depth_bias_enable: vk::FALSE,
+        depth_bias_slope_factor: 0.0,
+    };
+    let multisample_state_create_info = vk::PipelineMultisampleStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        flags: vk::PipelineMultisampleStateCreateFlags::empty(),
+        p_next: std::ptr::null(),
+        rasterization_samples: vk::SampleCountFlags::TYPE_1,
+        sample_shading_enable: vk::FALSE,
+        min_sample_shading: 0.0,
+        p_sample_mask: std::ptr::null(),
+        alpha_to_one_enable: vk::FALSE,
+        alpha_to_coverage_enable: vk::FALSE,
+    };
+
+    let stencil_state = vk::StencilOpState {
+        fail_op: vk::StencilOp::KEEP,
+        pass_op: vk::StencilOp::KEEP,
+        depth_fail_op: vk::StencilOp::KEEP,
+        compare_op: vk::CompareOp::ALWAYS,
+        compare_mask: 0,
+        write_mask: 0,
+        reference: 0,
+    };
+
+    let depth_state_create_info = vk::PipelineDepthStencilStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineDepthStencilStateCreateFlags::empty(),
+        depth_test_enable: vk::FALSE,
+        depth_write_enable: vk::FALSE,
+        depth_compare_op: vk::CompareOp::LESS_OR_EQUAL,
+        depth_bounds_test_enable: vk::FALSE,
+        stencil_test_enable: vk::FALSE,
+        front: stencil_state,
+        back: stencil_state,
+        max_depth_bounds: 1.0,
+        min_depth_bounds: 0.0,
+    };
+
+    let color_blend_attachment_states = [vk::PipelineColorBlendAttachmentState {
+        blend_enable: vk::FALSE,
+        color_write_mask: vk::ColorComponentFlags::all(),
+        src_color_blend_factor: vk::BlendFactor::ONE,
+        dst_color_blend_factor: vk::BlendFactor::ZERO,
+        color_blend_op: vk::BlendOp::ADD,
+        src_alpha_blend_factor: vk::BlendFactor::ONE,
+        dst_alpha_blend_factor: vk::BlendFactor::ZERO,
+        alpha_blend_op: vk::BlendOp::ADD,
+    }];
+
+    let color_blend_state = vk::PipelineColorBlendStateCreateInfo {
+        s_type: vk::StructureType::PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineColorBlendStateCreateFlags::empty(),
+        logic_op_enable: vk::FALSE,
+        logic_op: vk::LogicOp::COPY,
+        attachment_count: color_blend_attachment_states.len() as u32,
+        p_attachments: color_blend_attachment_states.as_ptr(),
+        blend_constants: [0.0, 0.0, 0.0, 0.0],
+    };
+
+    let pipeline_layout_create_info = vk::PipelineLayoutCreateInfo {
+        s_type: vk::StructureType::PIPELINE_LAYOUT_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineLayoutCreateFlags::empty(),
+        set_layout_count: 0,
+        p_set_layouts: std::ptr::null(),
+        push_constant_range_count: 0,
+        p_push_constant_ranges: std::ptr::null(),
+    };
+
+    let pipeline_layout = unsafe {
+        device
+            .create_pipeline_layout(&pipeline_layout_create_info, None)
+            .expect("Failed to create pipeline layout")
+    };
+
+    let graphic_pipeline_create_infos = [vk::GraphicsPipelineCreateInfo {
+        s_type: vk::StructureType::GRAPHICS_PIPELINE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: vk::PipelineCreateFlags::empty(),
+        stage_count: shader_stages.len() as u32,
+        p_stages: shader_stages.as_ptr(),
+        p_vertex_input_state: &vertex_input_state_create_info,
+        p_input_assembly_state: &vertex_input_assembly_state_info,
+        p_tessellation_state: std::ptr::null(),
+        p_viewport_state: &viewport_state_create_info,
+        p_rasterization_state: &rasterization_statue_create_info,
+        p_multisample_state: &multisample_state_create_info,
+        p_depth_stencil_state: &depth_state_create_info,
+        p_color_blend_state: &color_blend_state,
+        p_dynamic_state: std::ptr::null(),
+        layout: pipeline_layout,
+        render_pass,
+        subpass: 0,
+        base_pipeline_handle: vk::Pipeline::null(),
+        base_pipeline_index: -1,
+    }];
+
+    let graphics_pipelines = unsafe {
+        device
+            .create_graphics_pipelines(vk::PipelineCache::null(), &graphic_pipeline_create_infos, None)
+            .expect("Failed to create Graphics Pipeline!.")
+    };
+
     unsafe {
         device.destroy_shader_module(vertex_shader_module, None);
         device.destroy_shader_module(fragment_shader_module, None);
     }
+
+    (pipeline_layout, graphics_pipelines[0])
+}
+
+fn create_render_pass(device: &ash::Device, surface_format: vk::Format) -> vk::RenderPass {
+    let color_attachment = vk::AttachmentDescription {
+        flags: vk::AttachmentDescriptionFlags::empty(),
+        format: surface_format,
+        samples: vk::SampleCountFlags::TYPE_1,
+        load_op: vk::AttachmentLoadOp::CLEAR,
+        store_op: vk::AttachmentStoreOp::STORE,
+        stencil_load_op: vk::AttachmentLoadOp::DONT_CARE,
+        stencil_store_op: vk::AttachmentStoreOp::DONT_CARE,
+        initial_layout: vk::ImageLayout::UNDEFINED,
+        final_layout: vk::ImageLayout::PRESENT_SRC_KHR,
+    };
+
+    let color_attachment_ref = vk::AttachmentReference {
+        attachment: 0,
+        layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+    };
+
+    let subpass = vk::SubpassDescription {
+        flags: vk::SubpassDescriptionFlags::empty(),
+        pipeline_bind_point: vk::PipelineBindPoint::GRAPHICS,
+        input_attachment_count: 0,
+        p_input_attachments: std::ptr::null(),
+        color_attachment_count: 1,
+        p_color_attachments: &color_attachment_ref,
+        p_resolve_attachments: std::ptr::null(),
+        p_depth_stencil_attachment: std::ptr::null(),
+        preserve_attachment_count: 0,
+        p_preserve_attachments: std::ptr::null(),
+    };
+
+    let render_pass_attachments = [color_attachment];
+
+    let renderpass_create_info = vk::RenderPassCreateInfo {
+        s_type: vk::StructureType::RENDER_PASS_CREATE_INFO,
+        flags: vk::RenderPassCreateFlags::empty(),
+        p_next: std::ptr::null(),
+        attachment_count: render_pass_attachments.len() as u32,
+        p_attachments: render_pass_attachments.as_ptr(),
+        subpass_count: 1,
+        p_subpasses: &subpass,
+        dependency_count: 0,
+        p_dependencies: std::ptr::null(),
+    };
+
+    let render_pass = unsafe {
+        device
+            .create_render_pass(&renderpass_create_info, None)
+            .expect("Failed to create render pass!")
+    };
+
+    render_pass
 }
